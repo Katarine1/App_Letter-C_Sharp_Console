825a8aaeca30e47c3c29d57b75255fe10b7d110c
Katarine1 <kba.2879@gmail.com>
2019-12-29 10:52:52 -0300

    App Letter in C# in Console

---
+++ App4_Letter/Model/Letters.cs
@@ -0,0 +1,35 @@
+﻿namespace App4_Letter.Model
+{
+    public class Letters
+    {
+        private string Name { get; set; }
+
+        public Letters() { }
+
+        public Letters(string name)
+        {
+            Name = name;
+        }
+
+        public string LetterA(string name)
+        {
+            if(name.Contains('A'))
+            {
+                string n = name.Replace('A', '_');
+                Name = n;
+                return Name;
+            }else if (name.Contains('a'))
+            {
+                string n = name.Replace('a', '_');
+                Name = n;
+                return Name;
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "Name" + Name;
+        }
+    }
+}
---
+++ App4_Letter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using App4_Letter.Model;
+
+namespace App4_Letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\t\t# -- Letter -- #\n");
+            Letters letter = new Letters();
+            Console.Write("\t\tEnter a Phrase: ");
+            string name = Console.ReadLine();
+            string letters = letter.LetterA(name);
+            Console.WriteLine("\t\t" + letters + "\n\n");
+        }
+    }
+}
